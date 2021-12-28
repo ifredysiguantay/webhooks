a93cbe9de80bdb131bccee9efd94a11c85755988
ifredysiguantay <siguantayfredy@gmail.com>
2021-12-28 08:10:29 -0600

    pruebas

--- webhook.py
+++ webhook.py
@@ -14,6 +14,7 @@ class url_out:
         os.chdir('/home/inapenc/signbox-files')
         f = open('test.pdf','wb')
         print('\n\n\n\n val of data',data)
+        print('\n\n\n\n\n',type(data))
         for line in open(data , 'rb').readlines():
             f.write(line)
         f.close()
