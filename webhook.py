98ae8ecb6dd1716ed73728e21ba476168bc2da58
ifredysiguantay <siguantayfredy@gmail.com>
2021-12-28 08:19:23 -0600

    Pruebas

--- webhook.py
+++ webhook.py
@@ -12,10 +12,10 @@ class url_out:
     def POST(self):
         data = web.data()
         os.chdir('/home/inapenc/signbox-files')
-        f = open('test.pdf','w')
+        f = open('test.pdf','wb')
         print('\n\n\n\n val of data',data)
         print('\n\n\n\n\n',type(data))
-        for line in open(data , 'wb').readlines():
+        for line in open(data , 'rb').readlines():
             f.write(line)
         f.close()
         return ''
