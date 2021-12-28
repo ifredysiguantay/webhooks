b9454876ef37370496042c04a2c0131b29fd3b2a
ifredysiguantay <siguantayfredy@gmail.com>
2021-12-28 13:58:33 +0000

    Commit inicial

---
+++ webhook.py
@@ -0,0 +1,32 @@
+import web
+import os
+urls = (
+        '/result', 'url_out',
+        '/servicelogs', 'urlback'
+        )
+
+#app = web.application(urls, globals())
+#app = app.wsgifunc()
+
+class url_out:
+    def POST(self):
+        data = web.data()
+        os.chdir('/home/inapenc/signbox-files')
+        f = open('test.pdf','w')
+        f.write(data)
+        f.close()
+        return ''
+
+class urlback:
+    def POST(self):
+        data = web.data()
+        os.chdir('/home/inapenc/signbox-files/logs')
+        f = open('logs.txt', 'a+')
+        f.write(str(data))
+        f.write(str("\n"))
+        f.close()
+        return ''
+
+if __name__ == "__main__":
+    app = web.application(urls, globals())
+    app.run()
