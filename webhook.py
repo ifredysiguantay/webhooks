3c6125702dca7c0cdeff242629a1b18ae3521972
ifredysiguantay <siguantayfredy@gmail.com>
2022-01-06 15:20:12 -0600

    Cambios en webhook

--- webhook.py
+++ webhook.py
@@ -3,27 +3,30 @@ import web
 import base64
 bytes
 urls = (
-        '/result', 'url_out',
-        '/servicelogs', 'urlback'
+        '/result/(.+)/(.+)/(.+)', 'url_out',
+        '/servicelogs/(.+)', 'urlback'
         )
 
 #app = web.application(urls, globals())
 #app = app.wsgifunc()
 
 class url_out:
-    def POST(self):
+    def POST(self,dpi,edicion,documento):
         data = web.data()
-        os.chdir('/home/inapenc/signbox-files')
-        with open("test.pdf",'wb') as f:
+        if not os.path.exists('/home/inapdev/Docs/{}/{}'.format(dpi,edicion)):
+            os.makedirs('/home/inapdev/Docs/{}/{}'.format(dpi,edicion))
+            
+        os.chdir('/home/inapdev/Docs/{}/{}'.format(dpi,edicion))
+        with open('{}.pdf'.format(documento),'wb') as f:
             f.write(data)
         #f.close()
         return ''
 
 class urlback:
-    def POST(self):
+    def POST(self,name):
         data = web.data()
-        os.chdir('/home/inapenc/signbox-files/logs')
-        f = open('logs.txt', 'a+')
+        os.chdir('/home/inapdev/Docs/Logs')
+        f = open('{}.txt'.format(name), 'a+')
         f.write(str(data))
         f.write(str("\n"))
         f.close()
